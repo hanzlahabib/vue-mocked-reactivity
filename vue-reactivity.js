ee7a2ed8b73e98abc657728afa7abd6ab743967e
Hanzla habib <hanzla.habib@regits.com>
2020-06-05 00:30:36 +0500

    Mocking VueJs Reactivity in least simple way
    As we know javascript is not reactive to its variables, so i
    just learned way to make reactivity possible with in javaScript using some Dep class and object defineProperty to make use of getters and setters

---
+++ vue-reactivity.js
@@ -0,0 +1,46 @@
+let data = {price : 5, quantity : 2}
+let target, total, salePrice
+
+class Dep {
+  constructor(){
+    this.subscribers = []
+  }
+  depend(){
+    if(target && !this.subscribers.includes(target)){
+      this.subscribers.push(target)
+    }
+  }
+  notify(){
+    this.subscribers.forEach(sub => sub())
+  }
+}
+
+Object.keys(data).forEach(key => {
+  let internalValues = data[key]
+  const dep = new Dep()
+  Object.defineProperty(data, key, {
+    get(){
+      dep.depend()
+      return internalValues
+    },
+    set(newVal){
+      internalValues = newVal
+      dep.notify()
+    }
+  })
+})
+
+
+function watcher(myFunc){
+  target = myFunc
+  target()
+  target = null
+}
+
+watcher(() => {
+  total = data.price * data.quantity
+})
+
+watcher(() => {
+  salePrice = data.price * 0.9
+})
